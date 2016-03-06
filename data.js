89636dcc9a14abfe4366532f9ec20bc3c335eb89
Yumeng Song <ymeng.song@gmail.com>
2016-03-06 15:51:24 -0500

    Update for lab4.

--- data.js
+++ data.js
@@ -1,3 +1,48 @@
-var exports = module.exports = {};
+var myModuleToExport = module.exports = {};
 
-// You can now add export properties to the exports object to be accessible from outside this file
+myModuleToExport.retirementAmountIfSavingPerMonth = function (yearsUntilRetirement,
+    amountSavingPerMonth, yearlyInterestRateOfInvestment) {
+
+    var monthsUntilRetirement = yearsUntilRetirement * 12;
+    var i = 1;
+    var RunningTotal = amountSavingPerMonth;
+    while (i < monthsUntilRetirement) {
+        RunningTotal = (RunningTotal + amountSavingPerMonth) * (1 + yearlyInterestRateOfInvestment / 12);
+        i++;
+    }
+    return RunningTotal;
+}
+
+// console.log("How much retirement fund will you have, if saving 200 dollars per month for 30 years? (Interest: 3%)");
+// console.log(myModuleToExport.retirementAmountIfSavingPerMonth(30,200,0.03));
+
+myModuleToExport.investesAmountAfterSomeYears = function (yearsInvesting, initialAmount, yearlyInterestRateofInveatment) {
+
+    var k = 0;
+    var runningTotal = initialAmount;
+    while (k < yearsInvesting) {
+        runningTotal = runningTotal * (1 + yearlyInterestRateofInveatment);
+        k++;
+    }
+
+    return runningTotal;
+}
+
+// console.log("How much will you get from 1,000 dollars after 30 Years? (Interest: 3%)");
+// console.log(myModuleToExport.investesAmountAfterSomeYears(30,1000,0.03));
+
+myModuleToExport.monthsToPayOffLoan = function (monthlyPaymentAmount,
+    initialLoanAmount, yearlyInterestRateOfLoan) {
+    //console.log(monthlyPaymentAmount, initialLoanAmount, yearlyInterestRateOfLoan);
+    var leftToPay = initialLoanAmount;
+    var totalMonth = 0;
+    while (leftToPay > 0) {
+        leftToPay = (leftToPay * (1 + yearlyInterestRateOfLoan)) - monthlyPaymentAmount;
+        totalMonth++;
+    }
+
+    return totalMonth;
+}
+ 
+// console.log("How many months will it take to pay off 1500 dollars? (Interest: 8%)");
+//console.log(myModuleToExport.monthsToPayOffLoan(300, 1500, 0.08));
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ var myData = require('./data.js')
 // This package exports the function to create an express instance:
 var app = express();
 
+// Here we change our view engine from Jade (default) to EJS
+app.set('view engine', 'ejs');
+
 // This is called 'adding middleware', or things that will help parse your request
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
@@ -15,7 +18,131 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 // it will check the 'static' folder for matching files 
 app.use('/assets', express.static('static'));
 
+// check for hidden input with the tag _method
+// browser sometimes cannot submit a PUT or DELETE request,
+// so we can use middleware to change it before it hits our routes!
+// I have programatically injected some code that will add a hidden input called _method
+app.use(function (req, res, next) {
+    if (req.body && req.body._method) {
+        req.method = req.body._method;
+        delete req.body._method;
+    }
+
+    // let the next middleware run:
+    next();
+});
+
 // Setup your routes here!
+app.post("/results/perMonthRetirementSavings", function (request, response) {
+    try {
+        var years = request.body.years;
+        var perMonth = request.body.perMonth;
+        var interestRate = request.body.interestRate;
+        // var query = require('url').parse(request.url, true).query;
+        // var years = query.years;
+        // var perMonth = query.perMonth;
+        // var interestRate = query.interestRate;
+
+        var result = myData.retirementAmountIfSavingPerMonth(years, perMonth, interestRate);
+        if (!years || !perMonth || !interestRate || years < 0 || perMonth < 0 || interestRate < 0) {
+            response.status(500).render('pages/error', { errorType: "Invaild parameter(s)", errorMessage: 'Please check your input.' });
+        }
+        response.render('pages/results', { operationTitle: 'perMonthRetirementSavings', result: result });
+    } catch (message) {
+        response.status(500).render('pages/error', { errorType: "Issue loading question!", errorMessage: "message" });
+    }
+});
+
+app.post("/results/investedAmount", function (request, response) {
+    try {
+        var years = request.body.years;
+        var initial = request.body.initial;
+        var interestRate = request.body.interestRate;
+        var result = myData.investesAmountAfterSomeYears(years, initial, interestRate);
+        if (!years || !initial || !interestRate || years < 0 || initial < 0 || interestRate < 0) {
+            response.status(500).render('pages/error', { errorType: "Invaild parameter(s)", errorMessage: 'Please check your input.' });
+        }
+        response.render('pages/results', { operationTitle: 'investedAmount', result: result });
+    } catch (message) {
+        response.status(500).render('pages/error', { errorType: "Issue loading question!", errorMessage: message });
+    }
+});
+
+app.post("/results/loanPayoff", function (request, response) {
+    try {
+        var monthlyAmount = request.body.monthlyAmount;
+        var loanAmount = request.body.loanAmount;
+        var interestRate = request.body.interestRate;
+        var result = myData.monthsToPayOffLoan(monthlyAmount, loanAmount, interestRate);
+        if (!monthlyAmount || !loanAmount || !interestRate || monthlyAmount < 0 || loanAmount < 0 || interestRate < 0) {
+            response.status(500).render('pages/error', { errorType: "Invaild parameter(s)", errorMessage: 'Please check your input.' });
+        }
+        response.render('pages/results', { operationTitle: 'loanPayoff', result: result });
+    } catch (message) {
+        response.status(500).render('pages/error', { errorType: "Issue loading question!", errorMessage: message });
+    }
+});
+
+app.get("/api/perMonthRetirementSavings", function (request, response) {
+
+    var query = require('url').parse(request.url, true).query;
+    var years = query.years;
+    var perMonth = query.perMonth;
+    var interestRate = query.interestRate;
+
+    if (!years || !perMonth || !interestRate || years < 0 || perMonth < 0 || interestRate < 0) {
+        throw "Invaild parameter(s). Please check your input.";
+    }
+    try {
+        var result = myData.retirementAmountIfSavingPerMonth(years, perMonth, interestRate);
+        response.json({ status: "success", message: result });
+    } catch (e) {
+        response.status(500).json({ status: "error", message: e });
+    }
+});
+
+app.get("/api/investedAmount", function (request, response) {
+
+    var query = require('url').parse(request.url, true).query;
+    var years = query.years;
+    var initial = query.initial;
+    var interestRate = query.interestRate;
+
+    if (!years || !initial || !interestRate || years < 0 || initial < 0 || interestRate < 0) {
+        throw "Invaild parameter(s). Please check your input.";
+    }
+    try {
+        var result = myData.investesAmountAfterSomeYears(years, initial, interestRate);
+        response.json({ status: "success", message: result });
+    } catch (e) {
+        response.status(500).json({ status: "error", message: e });
+    }
+});
+
+app.get("/api/loanPayoff", function (request, response) {
+
+    var query = require('url').parse(request.url, true).query;
+    var monthlyAmount = query.monthlyAmount;
+    var loanAmount = query.loanAmount;
+    var interestRate = query.interestRate;
+
+    if (!monthlyAmount || !loanAmount || !interestRate || monthlyAmount < 0 || loanAmount < 0 || interestRate < 0) {
+        throw "Invaild parameter(s). Please check your input.";
+    }
+    try {
+        var result = myData.monthsToPayOffLoan(monthlyAmount, loanAmount, interestRate);
+        response.json({ status: "success", message: result });
+    } catch (e) {
+        response.status(500).json({ status: "error", message: e });
+    }
+});
+
+// app.get('*', function (request, response) {
+//     response.send('URI Format: '
+//     +'<br/>http://localhost:3000/api/perMonthRetirementSavings?years=NUMBER&perMonth=NUMBER&interestRate=NUMBER'
+//     +'<br/>http://localhost:3000/api/investedAmount?years=NUMBER&initial=NUMBER&interestRate=NUMBER'
+//     +'<br/>http://localhost:3000/api/loanPayoff?monthlyAmount=NUMBER&loanAmount=NUMBER&interestRate=NUMBER');
+// });
 
 app.get("/", function (request, response) { 
     // We have to pass a second parameter to specify the root directory
